26f43f65401e38f49c62eb6403f9286578a33133
OmarHerrrera <omar.herrera35@my.unitec.edu.mx>
2024-10-07 20:43:54 -0600

    bubble.cpp

--- vectores/codigo.cpp
+++ vectores/codigo.cpp
@@ -1,14 +1,66 @@
-#include <random>
+// ConsoleApplication12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <iostream>
 #include <vector>
-#include <chrono> 
+#include <random>
+#include <chrono>
+
+
 using namespace std;
 using namespace std::chrono;
 
-//funcion prototipo 
 
-void burbuja(vector < double > & v);
+//funcion prototipo
+void burbuja(vector<double>& v);
 
+void imprime(vector<double> v);
+int main()
+{
+    // vector con 1000 elementos
+    vector<double> v1000;
+    random_device rd;
+    default_random_engine gen(rd());
+    uniform_real_distribution<double>
+        distribution(1.5, 1.90);
+
+    // generar 1000 datos con una
+    // instrucción cíclico
+    for (int i = 0; i < 100000; i++)
+    {
+        double num = distribution(gen);
+        v1000.push_back(num);
+    }
+    //imprime(v1000);
+    auto start1000 = high_resolution_clock::now(); 
+    burbuja(v1000); 
+    auto stop1000 = high_resolution_clock::now();
+    auto duration1000 = duration_cast<milliseconds>(stop1000 - start1000);
+    cout << "Duracion " << duration1000.count() << endl;
+    //imprime(v1000); 
+
+}
+
+void burbuja(vector <double> & v )
+{
+    double temp = 0; 
+    bool swapp = true; 
+
+    while (swapp)
+    {
+        swapp = false; 
+        for (int i = 0; i < v.size()-1; i++)
+        {
+            if (v[i] > v[i + 1])
+            {
+                temp = v[i + 1]; 
+                v[i + 1] = v[i];
+                v[i] = temp;
+                swapp = true; 
+            }
+        }
+    }
+}
 
 
 void imprime(vector<double> v)
@@ -20,27 +72,4 @@ void imprime(vector<double> v)
     cout << endl;
     system("pause");
     system("cls");
-}
-
-
-int main()
-{
-    // vector con 1000 elementos 
-
-    vector <double> v1000; 
-    random_device rd; 
-    default_random_engine gen(rd());
-    uniform_real_distribution <double> distribution(1.5, 1.90); 
-
-
-    // generar 1000 datos con una instruccion ciclica 
-
-    for (int i = 0; i < 1000; i++)
-    {
-        double num = distribution(gen); 
-        v1000.push_back(num); 
-    }
-
-    imprime(v1000); 
-
 }
